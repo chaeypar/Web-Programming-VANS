7380e0bb72cee57fba18b5ac11b353c3df0aa1fa
chaeyeon <kidsland09@naver.com>
2023-02-05 17:58:40 +0900

    react-router-dom related work

--- vans/src/App.js
+++ vans/src/App.js
@@ -1,10 +1,20 @@
 import Navbar from './components/Navbar/NavbarPresenter';
+import Footbar from './components/Footbar/FootbarPresenter';
+import Main from './components/Main/MainPresenter';
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import QnA from './components/QnA/QnAPresenter';
+import Info from './components/Info/InfoPresenter'
 function App() {
   return (
     <div>
-      <Navbar/>
+        <Navbar/>
+        <Routes>
+          <Route path='*' element={<Main/>}/>
+          <Route path='/qna' element={<QnA/>}/>
+          <Route path='/info' element={<Info/>}/>
+        </Routes>
+        <Footbar/>
     </div>
   );
 }
