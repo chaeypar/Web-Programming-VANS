f6f283b1142c01da04561711664020319a487d16
woojin <woojin5555@gmail.com>
2023-02-05 22:37:31 +0900

    Add: user login, signup

--- vans_back/vans/user/models.py
+++ vans_back/vans/user/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+import secrets
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+import uuid
 
-# Create your models here.
+def generate_hex(nbytes=32):
+    return secrets.token_hex(nbytes)
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+    
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("Users must have an emmai address")
+        user = self.model(
+            emil=self.normalize_email(email),
+        )
+        user.ser_password(password)
+        user.save(using=self._db)  
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password=password)
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    
+    """
+    [User Model]
+    """
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          null=False, blank=False, auto_created=True)
+    email = models.EmailField(max_length=255, null=True, blank=True, unique=True)
+    phone_number = models.CharField(max_length=11, blank=True, null=True)
+    nickname = models.CharField(max_length=20, blank=True, null=True)
+    name = models.CharField(max_length=20, null=True, blank=True)
+    gender = models.CharField(max_length=1, null=True, blank=True)
+    birth_year = models.IntegerField(null=True, blank=True)
+    # profile_image = models.ImageField()
+    
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    shipping_name = models.CharField(max_length=20, null=True, blank=True)
+    shipping_phone_number = models.CharField(
+        max_length=11, null=True, blank=True)
+    shipping_zipcode = models.CharField(max_length=10, null=True, blank=True)
+    shipping_address1 = models.CharField(max_length=100, null=True, blank=True)
+    shipping_address2 = models.CharField(max_length=100, null=True, blank=True)
+    
+    objects = UserManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return f"{self.email} ({self.nickname})"
+    
+class AuthToken(models.Model):
+    id = models.CharField(default=generate_hex,
+                          max_length=64, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- vans_back/vans/user/views.py
+++ vans_back/vans/user/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from .models import User, AuthToken
+from .serializer import UserSerializer
 
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+
+class UserSignUp(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    
+    def create(self, request, *args, **kwargs):
+        if User.objects.filter(email=request.data['email']).exists():
+            return Response(data={"status": "FAILED", "message": "이미 존재하는 이름입니다."},
+                            status=status.HTTP_400_BAD_REQUEST)
+        if User.objects.filter(nickname=request.data['nickname']).exists():
+            return Response(data={"status": "FAILED", "message": "이미 존재하는 닉네임입니다."},
+                            status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user = serializer.instance
+        token = AuthToken.objects.create(user=user)
+        user.set_password(request.data.get('password'))
+        user.save()
+        return Response({'id': str(user.id), 'token': str(token.id)}, status=status.HTTP_201_CREATED)
+    
+class UserLogin(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        try:
+            user = User.objects.get(email=email) # stip().lower()?
+        except User.DoesNotExist:
+            return Response({'error':'존재하지 않는 유저입니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        if user.check_password(password):
+            token = AuthToken.objects.create(user=user)
+            serializer = self.get_serializer(data=user)
+            serializer.is_valid(raise_exception=True)
+            return Response({'token': str(token.id), 'info': serializer.data}, status=status.HTTP_200_OK)
+        else:
+        	return Response({'error':'비밀번호가 맞지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+  
+        
+  
+        
+        
+
