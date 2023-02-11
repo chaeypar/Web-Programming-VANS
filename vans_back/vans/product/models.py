c49bdc9c234ff4b838ae7e00b4722460d378f45a
woojin <woojin5555@gmail.com>
2023-02-11 11:35:53 +0900

    Add: product init

--- vans_back/vans/product/models.py
+++ vans_back/vans/product/models.py
@@ -1,4 +1,22 @@
 from django.db import models
 
 class Product(models.Model):
-    user = models.ForeignKey('user.User', related_name="products")
+    name = models.CharField()
+    # images 
+    price = models.PositiveIntegerField()
+    discount_rate = models.PositiveSmallIntegerField() # %단위 표기
+    delivery_cost = models.PositiveIntegerField()
+    inventory_by_size = models.JSONField()
+    
+    num = models.PositiveIntegerField()
+    status = models.Choices()
+    manufacturer = models.CharField()
+    model_code = models.CharField()
+    manufacturing_date = models.DateField()
+    origin = models.CharField()
+    extra_info = models.JSONField()
+    description = models.TextField()
+    tags = models.JSONField()
+    
+    
+    
